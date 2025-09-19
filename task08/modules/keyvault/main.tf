f1e2cd0a71ade600330027ba287cf4b56bd5b009
Sümeyye Ünal <90193242+sumpertramp@users.noreply.github.com>
2025-09-19 22:11:34 +0300

    Add files via upload

--- task08/modules/keyvault/main.tf
+++ task08/modules/keyvault/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_key_vault" "kv" {
   sku_name                   = "standard"
   purge_protection_enabled   = false
   soft_delete_retention_days = 7
-  enable_rbac_authorization   = false
+  enable_rbac_authorization  = false
   tags                       = var.tags
 }
 
